5ff33c991cbb25c5c24b4d9534299a810a56c418
刘国章 <guozhang.liu>
2018-11-12 14:38:13 +0800

    创建 springcloud-api

---
+++ springcloud-demo-api/src/main/java/com/lgz/springcloud/demo/entitis/Dept.java
@@ -0,0 +1,9 @@
+package com.lgz.springcloud.demo.entitis;
+
+/**
+ * Created by lgz on 2018/11/12.
+ */
+public class Dept {
+
+
+}
---
+++ src/test/java/com/lgz/springcloud/demo/utils/Test.java
@@ -0,0 +1,7 @@
+package com.lgz.springcloud.demo.utils;
+
+/**
+ * Created by lgz on 2018/11/12.
+ */
+public class Test {
+}
